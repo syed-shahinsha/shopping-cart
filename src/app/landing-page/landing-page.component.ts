738c035cfc27e9f5449e31e3e9cdd39baea6d0be
Syed <syedsha97.i@gmail.com>
2020-10-27 22:36:15 +0530

    initial landing page changes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
-  constructor() { }
-
+  prodObs : AngularFirestoreCollection<any>;
+  products : Observable<any>;
+  constructor(private afs:AngularFirestore) {
+    this.prodObs = afs.collection<any>('Product');
+    this.products = this.prodObs.valueChanges();
+   }
+  
   ngOnInit(): void {
+    // this.products.get("Product")._subscribe((res)=>{
+    //   console.log(res);
+      
   }
 
 }
