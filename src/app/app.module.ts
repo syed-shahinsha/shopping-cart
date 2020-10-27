f7a5a121a77d715aed5b1b900cd2a91c5e576a06
Syed <syedsha97.i@gmail.com>
2020-10-27 11:41:42 +0530

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
